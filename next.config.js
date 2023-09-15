8ec47346c776cb8470ad98803a0917f43929ca7d
Restyled.io <commits@restyled.io>
2023-09-15 13:16:52 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -3,54 +3,54 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source : '/(.*)',
-        headers : [
+        source: "/(.*)",
+        headers: [
           {
-            key : 'X-Frame-Options',
-            value : 'DENY',
+            key: "X-Frame-Options",
+            value: "DENY",
           },
           {
-            key : 'X-XSS-Protection',
-            value : '1; mode=block',
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
           {
-            key : 'X-Content-Type-Options',
-            value : 'nosniff',
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key : 'Referrer-Policy',
-            value : 'origin-when-cross-origin',
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
           },
         ],
       },
-    ]
+    ];
   },
   // add security headers to static files
   async headers() {
     return [
       {
-        source : '/(.*)',
-        headers : [
+        source: "/(.*)",
+        headers: [
           {
-            key : 'X-Frame-Options',
-            value : 'DENY',
+            key: "X-Frame-Options",
+            value: "DENY",
           },
           {
-            key : 'X-XSS-Protection',
-            value : '1; mode=block',
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
           {
-            key : 'X-Content-Type-Options',
-            value : 'nosniff',
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key : 'Referrer-Policy',
-            value : 'origin-when-cross-origin',
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
           },
         ],
       },
-    ]
+    ];
   },
-}
+};
 
-                   module.exports = nextConfig
+module.exports = nextConfig;
