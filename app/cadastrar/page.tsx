f0faa44f8262e1bad0437b18bcd38af1da9f4a89
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-10-21 14:07:34 -0300

    fix: update packages && typo in Cadastrar and Entrar
    Signed-off-by: Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>

--- app/cadastrar/page.tsx
+++ app/cadastrar/page.tsx
@@ -4,16 +4,7 @@ import React from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import {
-  TextInput,
-  Button,
-  Switch,
-  Divider,
-  Group,
-} from '@mantine/core';
-import { FaFacebook, FaGoogle } from 'react-icons/fa';
-
-import signUp, { signUpWithFacebook, signUpWithGoogle } from '../../firebase/auth/signup';
+import signUp, { signUpWithFacebook, signUpWithGoogle } from '@/firebase/auth/signup';
 
 export default function Page() {
   const router = useRouter();
@@ -40,36 +31,114 @@ export default function Page() {
   };
 
   return (
-    <div>
-      <Group grow mb="md" mt="md">
-        <Button onClick={handleFacebook} leftSection={<FaFacebook />}>Se inscrever com o Facebook
-        </Button>
-        <Button onClick={handleGoogle} rightSection={<FaGoogle />}>Se inscrever com o Google
-        </Button>
-      </Group>
-      <Divider label="Ou continue com o email" labelPosition="center" my="lg" />
-      <h1>Cadastrar</h1>
-      <form onSubmit={handleForm} className="form">
-        <TextInput
-          label="Seu Nome"
-          placeholder="Diogo Jorge Basso"
-          required
-          name="nome"
-          type="text"
-        />
-        <TextInput
-          label="Seu email"
-          placeholder="nome@email.com.br"
-          type="email"
-          name="email"
-          required
-        />
-        <button type="submit">Se inscrever</button>
-        <Switch
-          defaultChecked
-          label="Eu concordo com os termos e condições"
-        />
-      </form>
-    </div>
+    <section className="h-screen">
+      <div className="h-full">
+        <div
+          className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between"
+        >
+          <div
+            className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12"
+          >
+            <Image
+              src="/albatroz.png"
+              className="w-full"
+              alt="Sample image"
+              width={500}
+              height={500}
+            />
+          </div>
+
+          {/* Right column container */}
+          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+            <form>
+              {/*Sign in section*/}
+              <div
+                className="flex flex-row items-center justify-center lg:justify-start"
+              >
+                <h1 className="mb-0 mr-4 text-lg">Cadastre-se</h1>
+
+                {/* Facebook */}
+                <button
+                  type="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  onClick={handleFacebook}
+                  className="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mx-auto h-3.5 w-3.5"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+                    />
+                  </svg>
+                </button>
+
+                {/* Google */}
+                <button
+                  type="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  onClick={handleGoogle}
+                  className="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
+                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                  </svg>
+                </button>
+              </div>
+
+              {/* Separator between social media sign in and email/password sign in */}
+              <div
+                className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300"
+              >
+                <p
+                  className="mx-4 mb-0 text-center font-semibold dark:text-white"
+                >
+                  Ou
+                </p>
+              </div>
+
+              {/* Email input */}
+              <div className="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  type="text"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="exampleFormControlInput2"
+                  placeholder="Email address"
+                />
+                Email address
+              </div>
+
+              {/* Login button */}
+              <div className="text-center lg:text-left">
+                <button
+                  type="button"
+                  className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  onClick={handleForm}
+                >
+                  Login
+                </button>
+
+                {/* Register link */}
+                <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
+                  Não tem uma conta?
+                  <Link
+                    href="/cadastrar"
+                    className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
+                  >Quero me registrar
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- app/entrar/page.tsx
+++ app/entrar/page.tsx
@@ -5,8 +5,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { FaFacebook, FaGoogle } from 'react-icons/fa';
-
 import signIn, { signInWithGoogle, signInWithFacebook } from '@/firebase/auth/signin';
 
 export default function Page() {
@@ -58,7 +56,7 @@ export default function Page() {
               <div
                 className="flex flex-row items-center justify-center lg:justify-start"
               >
-                <p className="mb-0 mr-4 text-lg">Sign in with</p>
+                <h1 className="mb-0 mr-4 text-lg">Entrar com</h1>
 
                 {/* Facebook */}
                 <button
@@ -68,7 +66,6 @@ export default function Page() {
                   onClick={handleFacebook}
                   className="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                 >
-                  {/* Facebook */}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="mx-auto h-3.5 w-3.5"
@@ -89,16 +86,8 @@ export default function Page() {
                   onClick={handleGoogle}
                   className="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                 >
-                  {/* Linkedin */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mx-auto h-3.5 w-3.5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
+                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
                   </svg>
                 </button>
               </div>
@@ -110,7 +99,7 @@ export default function Page() {
                 <p
                   className="mx-4 mb-0 text-center font-semibold dark:text-white"
                 >
-                  Or
+                  Ou
                 </p>
               </div>
 
@@ -122,11 +111,7 @@ export default function Page() {
                   id="exampleFormControlInput2"
                   placeholder="Email address"
                 />
-                <label
-                  htmlFor="exampleFormControlInput2"
-                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                >Email address
-                </label>
+                Email address
               </div>
 
               {/* Login button */}
