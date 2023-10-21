ffaa0dd696675e64014cd84160865664326b06bb
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-10-21 10:38:41 -0300

    feat: add clients pictures
    Signed-off-by: Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { FaStar } from 'react-icons/fa';
+'use client';
+
 import Image from 'next/image';
 import YouTubeFrame from '@/components/Hero/YouTubeFrame';
 
 export default function Page() {
   return (
-    <main className="container mx-auto ">
+    <main className="mx-auto ">
       <section className="background-radial-gradient">
-
         <div className="px-6 py-12 text-center md:px-12 lg:text-left">
           <div className="container mx-auto">
             <div className="grid items-center gap-12 lg:grid-cols-2">
@@ -24,9 +24,9 @@ export default function Page() {
                   className="rounded-full "
                 >Faça um orçamento agora. É grátis.
                 </button>
-                <Image src="/gustavo-hermeto.jpg" height={32} width={32} alt="Gustavo Hermeto, atual presidente do DA" className="w-32 rounded-full" />
-                <Image src="/giovanna-leone.jpg" height={32} width={32} alt="Giovanna Leone, atual presidente do Albatroz" className="w-32 rounded-full" />
-                <Image src="/raquel-beyoha.jpg" height={32} width={32} alt="Raquel Beyoha, atual presidente do EPA" className="w-32 rounded-full" />
+                <Image src="/clientes/gustavo-hermeto.jpg" height={32} width={32} alt="Gustavo Hermeto, atual presidente do DA" className="w-32 rounded-full" />
+                <Image src="/clientes/giovanna-leone.jpg" height={32} width={32} alt="Giovanna Leone, atual presidente do Albatroz" className="w-32 rounded-full" />
+                <Image src="/clientes/raquel-beyoha.jpg" height={32} width={32} alt="Raquel Beyoha, atual presidente do EPA" className="w-32 rounded-full" />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>+30
@@ -46,8 +46,8 @@ export default function Page() {
           </div>
         </div>
         <div className="mb-32 text-center">
-          <h2 className="mb-16 text-3xl font-bold">
-            Mais de <u className="">30</u> projetos entregues
+          <h2 className="mb-16 text-3xl text-white font-bold">
+            Mais de 30 projetos entregues
           </h2>
 
           <div className="grid items-center gap-6 md:grid-cols-2 lg:grid-cols-4">
--- components/Menu/HeaderMenu.tsx
+++ components/Menu/HeaderMenu.tsx
@@ -89,10 +89,10 @@ export function HeaderMenu() {
             {/* Mefga Product Menu */}
             <ProductMegaMenu />
             {/* Team link */}
-            <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+            <li className="mb-4 lg:mb-0 lg:pr-2 dark:bg-slate-800 " data-te-nav-item-ref>
               <a
                 className="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                href="#"
+                href="/clientes"
                 data-te-nav-link-ref
               >Clientes
               </a>
@@ -101,7 +101,7 @@ export function HeaderMenu() {
             <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
               <a
                 className="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                href="#"
+                href="/sobre"
                 data-te-nav-link-ref
               >Sobre nós
               </a>
