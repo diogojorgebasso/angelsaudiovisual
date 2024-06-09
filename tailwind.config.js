e0f3716e4dbd0255fb03c89c63a66dcde9e2905e
Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>
2024-06-09 17:14:23 -0300

    fixing firebase.json removing package ads test
    Signed-off-by: Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
